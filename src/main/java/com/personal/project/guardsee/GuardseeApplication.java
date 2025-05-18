c1fbaa2f97e9ac5484b4ad5a18dfce2e23ab8d5c
pafesan <pablof9610@gmail.com>
2025-05-17 23:21:01 -0300

    initial commit

---
+++ src/main/java/com/personal/project/guardsee/GuardseeApplication.java
@@ -0,0 +1,13 @@
+package com.personal.project.guardsee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardseeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/project/guardsee/GuardseeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.project.guardsee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardseeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
