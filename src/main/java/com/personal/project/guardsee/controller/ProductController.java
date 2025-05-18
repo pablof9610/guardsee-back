00f8a9e76de7bc46c270f0cb0a4825afbf621708
pafesan <pablof9610@gmail.com>
2025-05-17 23:22:16 -0300

    feat: adding controller folder and endpoints

---
+++ src/main/java/com/personal/project/guardsee/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.personal.project.guardsee.controller;
+
+import com.personal.project.guardsee.model.Product;
+import com.personal.project.guardsee.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping
+    public Long saveProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<Product> getAllProduct() {
+        return productService.getAllProduct();
+    }
+}
