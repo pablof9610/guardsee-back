73677760f33c02cf68f8e2a89ff13e779294f0f2
pafesan <pablof9610@gmail.com>
2025-05-20 22:35:28 -0300

    feat: add custom query for listing distinct count of products

--- src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
+++ src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.personal.project.guardsee.repository;
 
 import com.personal.project.guardsee.model.Product;
+import com.personal.project.guardsee.model.ProductListDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT new com.personal.project.guardsee.model.ProductListDTO(p.name, COUNT(p)) FROM Product p GROUP BY p.name")
+    List<ProductListDTO> countProductByName();
 }
