07b0fd961ff23d548039e208c5e1ec31bd0d2838
pafesan <pablof9610@gmail.com>
2025-05-25 21:06:00 -0300

    feat: add find producy by serialnumber for logic checks

--- src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
+++ src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
@@ -14,4 +14,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<ProductListDTO> countProductByName();
 
     int deleteBySerialNumber(String serialNumber);
+
+    Optional<Product> findProductBySerialNumber(String serialNumber);
 }
