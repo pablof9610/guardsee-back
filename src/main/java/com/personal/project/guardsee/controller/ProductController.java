ed7f0ef46c55a3e0c41b2d849d657ad1da32f6a0
pafesan <pablof9610@gmail.com>
2025-05-28 22:14:21 -0300

    chore: new imports

--- src/main/java/com/personal/project/guardsee/controller/ProductController.java
+++ src/main/java/com/personal/project/guardsee/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.personal.project.guardsee.model.Product;
 import com.personal.project.guardsee.model.ProductStateDTO;
 import com.personal.project.guardsee.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
