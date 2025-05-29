2e56ce386d0f4e6fa17d1160c03355031c43fdc0
pafesan <pablof9610@gmail.com>
2025-05-28 22:14:50 -0300

    chore: removing unused declaration

--- src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
+++ src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
@@ -4,7 +4,6 @@ import com.personal.project.guardsee.model.Product;
 import com.personal.project.guardsee.model.ProductListDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
