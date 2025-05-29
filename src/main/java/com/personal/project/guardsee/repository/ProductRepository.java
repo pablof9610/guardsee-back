792dda12e9e284215da983046b8cf0073de5864d
pafesan <pablof9610@gmail.com>
2025-05-28 22:14:53 -0300

    chore: removing unused declaration

--- src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
+++ src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-//select name, state, count(state) from tb_product group by name;
     @Query("SELECT new com.personal.project.guardsee.model.ProductListDTO(p.name, p.state, COUNT(p.state)) FROM Product p GROUP BY p.name, p.state ORDER BY p.name")
     List<ProductListDTO> countProductByName();
 
