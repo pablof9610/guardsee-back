f63cefdc5a585e16e1ce397a28ffb99ebec50a63
pafesan <pablof9610@gmail.com>
2025-05-28 22:13:39 -0300

    feat: add optional object to avoid NullPointerException

--- src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
+++ src/main/java/com/personal/project/guardsee/repository/ProductRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 //select name, state, count(state) from tb_product group by name;
